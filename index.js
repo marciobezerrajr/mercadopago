ec6dceb93f61b5d736822ca9c0eb76ee9a7d6ab0
Márcio <marcioalvesbj@gmail.com>
2021-10-31 23:09:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const MercadoPago = require('mercadopago')
+const app = express()
+
+MercadoPago.configure({
+    sandbox: true, //
+    access_token: 'TEST-88230570437118-110101-963b3c1076ba216cfec31f44cbef00d3-152688826'
+})
+
+app.get('/', (req, res) => {
+    res.send("Olá mundo")
+})
+
+app.get('/payment', async (req, res) => {
+
+    var id = "" + Date.now()
+    var payerMail = 'marcio@email.com'
+
+    const data = {
+        items: [
+            item = {
+                id: id,                                    //uuid --- Date.now
+                title: 'Poderia ser uma variável dos params ou do banco',
+                quantity: 1,
+                currency_id: 'BRL',
+                unit_price: parseFloat(150)
+            }
+        ],
+
+        payer: {
+            email: payerMail
+        },
+        external_reference: id,
+    }
+    try {
+        var pagto = await MercadoPago.preferences.create(data)
+        console.log(pagto)
+        //banco.salvarPagemento({id: id, pagador: emailpagador})
+        return res.redirect(pagto.body.init_point)
+    } catch (err) {
+        res.send(err.message)
+    }
+})
+
+app.listen(3000, (req, res) => {
+    console.log('Server on')
+})
